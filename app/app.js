420c643e621269092523b7cc68f85173bcbaacc7
BharanitharanK <bharanitharan1117@live.com>
2018-03-05 15:46:53 +0530

    app

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var express=require('express');
 var bodyParser = require('body-parser');
 var app=express();
 app.use(express.static('app'));
+app.use(express.static('web'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/web/html/welcome.html');
