c9ac8860be1ee48e1118afb244de07ebb1962488
BharanitharanK <bharanitharan1117@live.com>
2018-03-05 11:56:01 +0530

    app

--- app/app.js
+++ app/app.js
@@ -1,22 +1,13 @@
 var express=require('express');
-var pug=require('pug');
+var bodyParser = require('body-parser');
 var app=express();
-app.set('view engine', 'pug');
 app.use(express.static('app'));
-var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
 app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index1.html');
+  res.sendFile(__dirname+'/web/html/welcome.html');
 });
-app.post('/submit',function(req,res){
-  res.render('view', {
-      id:req.body.id,
-      name: req.body.name,
-      department:req.body.department,
-      salary:req.body.salary
-   });
+app.post('/login',function(req,res){
+  res.send(req.body);
 });
 app.get('/view',function(req,res){
 res.sendFile(__dirname+'/view.html')
