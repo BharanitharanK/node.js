23a1f91809d3f5da5d3d8338dad289ccaa25902c
BharanitharanK <bharanitharan1117@live.com>
2018-03-06 12:59:19 +0530

    cookie

--- app/app.js
+++ app/app.js
@@ -1,10 +1,15 @@
 var express=require('express');
 var bodyParser = require('body-parser');
-
+var cookie=require('cookie-session');
 var app=express();
 app.use(express.static('app'));
 app.use(express.static('web'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookie({
+  name:'session',
+  keys:'authentication',
+  maxAge:60 * 60 * 1000
+}))
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/web/html/welcome.html');
 });
