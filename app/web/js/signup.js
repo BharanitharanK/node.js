6c1cf6a9ea8181d4a8694f727f600aad9b478f22
BharanitharanK <bharanitharan1117@live.com>
2018-03-06 15:31:56 +0530

    cookie

---
+++ app/web/js/signup.js
@@ -0,0 +1,23 @@
+function validate()
+{
+var email = document.getElementById('email');
+var password1 = document.getElementById('password1').value;
+var password2 = document.getElementById('password2').value;
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!filter.test(email.value)) {
+    alert('Please provide a valid email address');
+    email.focus;
+    return false;
+    }
+    else if(password1.length<7){
+    alert('password must be more than 6 characters');
+    password.focus;
+    return false;
+   }
+    else if(password1===password2){
+    alert('password mismatch');
+    password.focus;
+    return false;
+    }
+}
